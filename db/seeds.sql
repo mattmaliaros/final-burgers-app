0513435f1c24343fc0b1381112f0d2950bed6955
Matt <mattmaliaros@gmail.com>
2019-08-20 17:38:53 -0400

    final push

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger', false);
+
